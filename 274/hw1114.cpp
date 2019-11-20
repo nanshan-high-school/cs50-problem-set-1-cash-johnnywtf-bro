d1850a2e6147bfd14f42292c2fd5a50408c0d55b
johnnywtf-bro <57487085+johnnywtf-bro@users.noreply.github.com>
2019-11-20 21:26:58 +0800

    hw1114
    27423

---
+++ 274/hw1114.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int money;
+    cout << "多少錢";
+    cin >> money;
+    while (true) { 
+        cout << (money / 1000) << "個1000" << "\n";
+        cout << ((money % 1000) / 100) << "個100" << "\n";
+        cout << (((money % 1000) % 100) / 10) << "個10" << "\n";
+        cout << ((((money % 1000) % 100) % 10) / 1) << "個1" << "\n";
+        if (((((money % 1000) % 100) % 10) / 1) == 0) {
+            break;
+        } else {
+            continue;
+        }    
+    }
+    }
